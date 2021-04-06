d6820cdbeca4f9bedd171badfb90fbb645fb4993
JulaRaluca <raluca.jula@student.upt.ro>
2021-04-06 22:08:57 +0300

    Initial commit

---
+++ src/main/java/net/atlassin/teamioanaraluca/Controllers/RegistrationController.java
@@ -0,0 +1,36 @@
+package net.atlassin.teamioanaraluca.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import net.atlassin.teamioanaraluca.Exceptions.UsernameAlreadyExistsException;
+import net.atlassin.teamioanaraluca.Services.UserService;
+
+public class RegistrationController {
+
+    @FXML
+    private Text registrationMessage;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private ChoiceBox role;
+
+    @FXML
+    public void initialize() {
+        role.getItems().addAll("Dentist", "Customer");
+    }
+
+    @FXML
+    public void handleRegisterAction() {
+        try {
+            UserService.addUser(usernameField.getText(), passwordField.getText(), (String) role.getValue());
+            registrationMessage.setText("Account created successfully!");
+        } catch (UsernameAlreadyExistsException e) {
+            registrationMessage.setText(e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/net/atlassin/teamioanaraluca/Main.java
@@ -0,0 +1,36 @@
+package net.atlassin.teamioanaraluca;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import net.atlassin.teamioanaraluca.Services.FileSystemService;
+import net.atlassin.teamioanaraluca.Services.UserService;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        initDirectory();
+        UserService.initDatabase();
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("register.fxml"));
+        primaryStage.setTitle("Registration Example");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+    private void initDirectory() {
+        Path applicationHomePath = FileSystemService.APPLICATION_HOME_PATH;
+        if (!Files.exists(applicationHomePath))
+            applicationHomePath.toFile().mkdirs();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
