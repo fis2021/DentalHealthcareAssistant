c35bff06d9b0c1cc8493d542a4cc1e5fd93ff7c8
JulaRaluca <raluca.jula@student.upt.ro>
2021-05-08 16:59:49 +0300

    DHA-23 + DHA-28 + DHA-32 Created the linkage between the Dentist GUI and the My services GUI

---
+++ src/main/java/net/atlassin/teamioanaraluca/Controllers/DentistController.java
@@ -0,0 +1,29 @@
+package net.atlassin.teamioanaraluca.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import net.atlassin.teamioanaraluca.Exceptions.UsernameDoesNotExistException;
+import net.atlassin.teamioanaraluca.Exceptions.WrongPasswordException;
+import net.atlassin.teamioanaraluca.Exceptions.WrongRoleException;
+import net.atlassin.teamioanaraluca.Services.UserService;
+
+public class DentistController {
+
+    @FXML
+    public void handleMyServicesAction(javafx.event.ActionEvent myServices) throws Exception {
+        Parent root1 = FXMLLoader.load(getClass().getClassLoader().getResource("MyServicesGUI.fxml"));
+        Stage window = (Stage) ((Node) myServices.getSource()).getScene().getWindow();
+        ;
+        window.setTitle("My Services");
+        window.setScene(new Scene(root1, 600, 406));
+        window.show();
+    }
+}
