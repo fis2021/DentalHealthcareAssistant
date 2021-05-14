ba254fe6c70e3359e1b056f71d1f0dd16b1d2e56
Ioana Lazea <lazeaioanabianca@gmail.com>
2021-05-14 17:07:07 +0300

    Displayed info for patient who is appointed today when doctor click on their username

--- src/main/java/net/atlassin/teamioanaraluca/Controllers/TodaysAppointmentsController.java
+++ src/main/java/net/atlassin/teamioanaraluca/Controllers/TodaysAppointmentsController.java
@@ -2,18 +2,23 @@ package net.atlassin.teamioanaraluca.Controllers;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import net.atlassin.teamioanaraluca.Model.Appointment;
 import net.atlassin.teamioanaraluca.Model.DentistServices;
+import net.atlassin.teamioanaraluca.Model.User;
 import net.atlassin.teamioanaraluca.Model.WhoIsLoggedInfo;
 import net.atlassin.teamioanaraluca.Services.AppointmentsService;
 import net.atlassin.teamioanaraluca.Services.DentistFacilitiesService;
+import net.atlassin.teamioanaraluca.Services.UserService;
 import org.dizitart.no2.objects.ObjectRepository;
 
 import java.io.IOException;
@@ -22,11 +27,49 @@ import java.time.format.DateTimeFormatter;
 
 public class TodaysAppointmentsController {
     private static ObjectRepository<Appointment> appointmentsRepository = AppointmentsService.getAppointmentsRepository();
+
     @FXML
     public ListView<String> todaysAppointmentListView = new ListView<String>();
+    @FXML
+    public Text appointmentInfo;
 
     public void initialize() throws IOException {
         updateWithTodaysAppointmentsListView();
+        todaysAppointmentListView.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+            @Override
+            public void handle(MouseEvent event) {
+                String usernamePatientClicked = todaysAppointmentListView.getSelectionModel().getSelectedItem().toString();
+                appointmentInfo.setText(getInfoPatient(usernamePatientClicked));
+            }
+        });
+    }
+
+    public String getInfoPatient(String usernamePatient){
+        String information = "";
+        //go through user repository
+        for(User user:UserService.userRepository.find()){
+            if (user.getRole().equals("Customer") && user.getUsername().equals(usernamePatient)){
+                information = information + "Name: "+user.getName();
+                information = information + "\nE-mail: "+user.getEmail();
+                information = information + "\nPhone: "+user.getPhoneNumber();
+            }
+        }
+
+
+
+        //go through appointment repository
+        String currentDate = returnCurrentDate();
+        for (Appointment appointment : appointmentsRepository.find()) {
+            if (appointment.getUsernamePatient().equals(usernamePatient) && appointment.getUsernameDoctor().equals(WhoIsLoggedInfo.getLoggedUsername()))
+                if (appointment.getStatus().equals("accepted"))
+                    if (appointment.getDate().equals(currentDate)) {
+                        information = information + "\nDate: "+appointment.getDate();
+                       information = information + "\nHour: "+appointment.getHour();
+                    }
+        }
+
+        return information;
     }
 
     public void updateWithTodaysAppointmentsListView(){
@@ -47,6 +90,17 @@ public class TodaysAppointmentsController {
                     todaysAppointmentListView.setItems(items);
             }
         }
+    }
+
+    public String returnCurrentDate(){
+        LocalDate currentDate = LocalDate.now();
+        int currentDay = currentDate.getDayOfMonth();
+        int currentMonth = currentDate.getMonthValue();
+        int currentYear = currentDate.getYear();
+        LocalDate ld = LocalDate.of( currentYear , currentMonth, currentDay );
+        DateTimeFormatter f = DateTimeFormatter.ofPattern( "dd/MM/uuuu" );
+        String dateFormat = ld.format( f );
+        return dateFormat;
     }
 
     public void goBackToDentistGUI(javafx.event.ActionEvent goToDentist) throws IOException {
